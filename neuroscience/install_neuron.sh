21f2ad9d9b9f58dff0bd5bbd223565fb2086f7fe
Jonathan Villemaire-Krajden <jonathan.villemaire-krajden@epfl.ch>
2020-03-05 17:29:51 +0100

    Rebuild for MPI

--- neuroscience/install_neuron.sh
+++ neuroscience/install_neuron.sh
@@ -8,12 +8,12 @@ wget -q -O /tmp/nrn.tar.gz https://neuron.yale.edu/ftp/neuron/versions/v7.7/7.7.
 tar -xvzf nrn.tar.gz
 
 cd nrn-${NEURON_VERSION}
-CC=/usr/lib64/mpich/bin/mpicc ./configure \
-                              --without-iv \
-                              --with-paranrn=dynamic \
-                              --with-readline=yes \
-                              --with-nrnpython=dynamic \
-                              --prefix=$NEURONHOME
+./configure \
+    --without-iv \
+    --with-paranrn=dynamic \
+    --with-readline=yes \
+    --with-nrnpython=dynamic \
+    --prefix=$NEURONHOME
 make -j install
 
 rm -rf /tmp/nrn*
